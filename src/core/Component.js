560bdb5fa645f3537d6c69fd6b340050c926a6c6
Ilya <iosdsbb@gmail.com>
2025-10-11 07:26:56 +0300

    feat: added observe logic to Component

--- src/core/Component.js
+++ src/core/Component.js
@@ -13,12 +13,36 @@ export class Component {
 
   constructor(props = {}) {
     this.props = props;
-    this.state = {};
+    this.state = this.observeState({});
     this.$rootElement = null;
     if (this.constructor === Component) {
       throw new Error("Cannot instantiate abstract class");
     }
     this.setup(props);
+  }
+
+  observeState(init) {
+    return new Proxy(init, {
+      set: (target, prop, val) => {
+        console.log(target, prop, val);
+        const oldVal = target[prop];
+        if (oldVal !== val) {
+          target[prop] = val;
+          this.onStateChange(prop, val, oldVal);
+        }
+        return true;
+      }
+    });
+  }
+
+  /**
+   * Вызывается при изменении state, переопределяется в дочерних классах
+   * @param {string} property
+   * @param {*} newVal
+   * @param {*} oldVal
+   */
+  onStateChange(property, newVal, oldVal) {
+
   }
 
   setup() {
