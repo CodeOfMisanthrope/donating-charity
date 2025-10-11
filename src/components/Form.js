ca49af6b6da18b4f44590fb3132d3cc669f94841
Ilya <iosdsbb@gmail.com>
2025-10-11 19:57:40 +0300

    feat: added logic submit for Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,19 @@
 import {Component} from "~core/Component.js";
 import Input from "~components/UI/Input.js";
 import BtnSubmit from "~components/UI/BtnSubmit.js";
+import {dateConvert} from "~utils/Date.js";
+
+/**
+ * @typedef Donate
+ * @property {string} dollars
+ * @property {string} dateTime
+ */
 
 /**
  * @typedef FormState
  * @property {Input} inputNumber
  * @property {BtnSubmit} btnSubmit
+ * @property {Donate} donate
  */
 
 /**
@@ -13,34 +21,65 @@ import BtnSubmit from "~components/UI/BtnSubmit.js";
  * @extends Component
  */
 export class Form extends Component {
+  // /**
+  //  * @type FormState
+  //  */
+  // state;
+
   constructor(props) {
     super(props);
+    // this.state = {};
+    // const inputNumber = new Input({
+    //   labelText: "Введите сумму в $",
+    //   type: "number",
+    //   name: "amount"
+    // });
+    // this.state.inputNumber = inputNumber;
+    //
+    // const btnSubmit = new BtnSubmit({
+    //   text: "Задонатить",
+    //   isActive: false
+    // });
+    // this.state.btnSubmit = btnSubmit;
+
+    // this.state = {
+    //   inputNumber: new Input({
+    //     labelText: "Введите сумму в $",
+    //     type: "number",
+    //     name: "amount"
+    //   }),
+    //
+    //   btnSubmit: new BtnSubmit({
+    //     text: "Задонатить",
+    //     isActive: false
+    //   })
+    // };
   }
 
   setup(props) {
     this.$rootElement = document.createElement('form');
     this.$rootElement.className = 'donate-form';
 
-    /**
-     * @type FormState
-     */
-    this.state = {};
-
     const inputNumber = new Input({
       labelText: "Введите сумму в $",
       type: "number",
       name: "amount"
     });
-    this.state.inputNumber = inputNumber;
     this.$rootElement.appendChild(inputNumber.$rootElement);
+    this.state.inputNumber = inputNumber;
 
     const btnSubmit = new BtnSubmit({
       text: "Задонатить",
       isActive: false
     });
-    this.state.btnSubmit = btnSubmit;
     this.$rootElement.appendChild(btnSubmit.$rootElement);
-    console.log(this.state);
+    this.state.btnSubmit = btnSubmit;
+
+    this.state.donate = {
+      dollars: '',
+      dateTime: ''
+    };
+
     this.initEvents(inputNumber.$rootElement, btnSubmit.$rootElement);
   }
 
@@ -54,9 +93,16 @@ export class Form extends Component {
   handleInput(evt) {
     const val = evt.target.value;
     this.state.btnSubmit.state.isActive = val.length > 0;
+    this.state.inputNumber.state.val = val;
   }
 
   handleSubmit(evt) {
+    evt.preventDefault();
+    const val = this.state.inputNumber.state.val;
+    const date = dateConvert(new Date());
+    const data = {val, date};
+    console.log(this.state.donate)
+    console.log(data);
     console.log('submit', evt.target);
   }
 }
