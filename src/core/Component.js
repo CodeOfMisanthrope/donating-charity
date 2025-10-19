00770e011ee52c2995b6d5fc02f370b129199850
Ilya <iosdsbb@gmail.com>
2025-10-19 15:32:13 +0300

    feat: updated Component

--- src/core/Component.js
+++ src/core/Component.js
@@ -2,10 +2,6 @@
  * Класс базовой логики компонента, от которого наследуется каждый компонент.
  */
 export class Component {
-  props;
-
-  state;
-
   /**
    * @type HTMLElement
    */
@@ -13,36 +9,12 @@ export class Component {
 
   constructor(props = {}) {
     this.props = props;
-    this.state = this.observeState({});
+    this.state = {};
     this.$rootElement = null;
     if (this.constructor === Component) {
       throw new Error("Cannot instantiate abstract class");
     }
     this.setup(props);
-  }
-
-  observeState(init) {
-    return new Proxy(init, {
-      set: (target, prop, val) => {
-        // console.log(target, prop, val);
-        const oldVal = target[prop];
-        if (oldVal !== val) {
-          target[prop] = val;
-          this.onStateChange(prop, val, oldVal);
-        }
-        return true;
-      }
-    });
-  }
-
-  /**
-   * Вызывается при изменении state, переопределяется в дочерних классах
-   * @param {string} property
-   * @param {*} newVal
-   * @param {*} oldVal
-   */
-  onStateChange(property, newVal, oldVal) {
-
   }
 
   setup() {
