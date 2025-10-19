ee9a2f25816454ce95b0fe768fc78e70b14ce5ee
Ilya <iosdsbb@gmail.com>
2025-10-19 15:44:14 +0300

    fix: updated App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import {Component} from "~core/Component.js";
 import {Form} from "~components/Form.js";
 import {List} from "~components/List.js";
 import { ListItem } from '~components/ListItem.js';
+import {dateConvert} from "~utils/Date.js";
 
 export class App extends Component {
   setup(props) {
@@ -10,13 +11,20 @@ export class App extends Component {
 
     // ...
     
-    const donateForm = new Form();
+    const donateForm = new Form({
+      onItemCreate: this.onItemCreate.bind(this),
+    });
+    this.state.form = donateForm;
     this.$rootElement.appendChild(donateForm.$rootElement);
     const donateList = new List();
+    this.state.list = donateList;
     this.$rootElement.appendChild(donateList.$rootElement);
   }
   
   onItemCreate(amount) {
     // ...
+    const date = dateConvert(new Date());
+    const data = {amount, date};
+    this.state.list.addItem(data);
   }
 }
