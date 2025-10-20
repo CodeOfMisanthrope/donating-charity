cdbd924fe71cdd323ae4d99651ca6d5bb3be3ef1
Ilya <iosdsbb@gmail.com>
2025-10-20 14:53:20 +0300

    feat: updated App

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,13 @@ import {dateConvert} from "~utils/Date.js";
 
 export class App extends Component {
   setup(props) {
+    this.state.amount = 0;
     this.$rootElement = document.createElement('div');
     this.$rootElement.className = 'app';
 
     // ...
-    
+    this.state['$title'] = this.createDOMTitle();
+    this.renderTitle(this.$rootElement, this.state['$title']);
     const donateForm = new Form({
       onItemCreate: this.onItemCreate.bind(this),
     });
@@ -26,5 +28,23 @@ export class App extends Component {
     const date = dateConvert(new Date());
     const data = {amount, date};
     this.state.list.addItem(data);
+    this.state.amount += Number(amount);
+    this.updateAmountTitle();
+  }
+
+  renderTitle($container, $title) {
+    $container.appendChild($title);
+  }
+
+  createDOMTitle() {
+    const $title = document.createElement("h1");
+    $title.classList.add('total-amount');
+    // по-другому хз как
+    $title.innerHTML = `Итого: $<span>${this.state.amount}</span>`;
+    return $title;
+  }
+
+  updateAmountTitle() {
+    this.state['$title'].innerHTML = `Итого: $<span>${this.state.amount}</span>`;
   }
 }
